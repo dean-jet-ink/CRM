9e43ecacdb77c6ef4bd785a816cf80aea914a1ed
unknown <rr7i5fvv@gmail.com>
2024-03-17 11:59:23 +0900

    初期コミット

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <p>Inertia Test</p>
+    <div>
+        <a href="/">a tag</a>
+    </div>
+    <Link href="/">Link tag</Link>
+</template>
